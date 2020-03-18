50f3fbd47e2a45f8389bd02e1d8a6e5f0913ba8d
BR3TT03 <rockbrett03@gmail.com>
2020-03-18 14:40:54 +0545

    Achieved live chat

--- src/Client.java
+++ src/Client.java
@@ -4,40 +4,48 @@ import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Client extends JFrame {
-	JTextArea message;
-	JTextField write;
-	JScrollPane jp;
-	static String cmsg="";
-	public Client()
-	{
-		setBounds(400,100,500,500);
-		getContentPane().setBackground(Color.blue);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(null);
-		message=new JTextArea();
-		jp=new JScrollPane(message);
-		jp.setBounds(50,50,400,200);
-		write=new JTextField();
-		write.setBounds(50, 270, 300, 30);
-		add(jp);
-		add(write);
-		setVisible(true);
-	}
+public class Client {
+//	JTextArea message;
+//	JTextField write;
+//	JScrollPane jp;
+//	static String cmsg="";
+//	public Client()
+//	{
+//		setBounds(400,100,500,500);
+//		getContentPane().setBackground(Color.blue);
+//		setDefaultCloseOperation(EXIT_ON_CLOSE);
+//		setLayout(null);
+//		message=new JTextArea();
+//		jp=new JScrollPane(message);
+//		jp.setBounds(50,50,400,200);
+//		write=new JTextField();
+//		write.setBounds(50, 270, 300, 30);
+//		add(jp);
+//		add(write);
+//		setVisible(true);
+//	}
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		new Client();
+		String ClientMessage;
+		Scanner input  = new Scanner(System.in);
 		Socket s= new Socket("Localhost",777);
+		while(true) {
 		PrintWriter pw= new PrintWriter(s.getOutputStream());
 		InputStreamReader in= new InputStreamReader(s.getInputStream());
 		BufferedReader br= new BufferedReader(in);
-		pw.println("Hi!");
+		System.out.print("Client:");
+		ClientMessage = input.nextLine();
+		pw.println(ClientMessage);
 		pw.flush();
 		String msg=br.readLine();
-		System.out.println("Server: "+msg);
-		pw.close();
-		br.close();
-		s.close();
+		if(msg!=null) {
+		System.out.println("Server:"+msg);
+		}
+//		pw.close();
+//		br.close();
+		}
+		//s.close();
 	}
 
 }
--- src/Server.java
+++ src/Server.java
@@ -4,60 +4,69 @@ import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Server extends JFrame  implements KeyListener {
-	JTextArea message;
-	JTextField write;
-	JScrollPane jp;
-	static String cmsg="";
-	public Server()
-	{
-		setBounds(400,100,500,500);
-		getContentPane().setBackground(Color.blue);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLayout(null);
-		message=new JTextArea();
-		jp=new JScrollPane(message);
-		jp.setBounds(50,50,400,200);
-		write=new JTextField();
-		write.setBounds(50, 270, 300, 30);
-		add(jp);
-		add(write);
-		setVisible(true);
-	}
+public class Server{
+//	JTextArea message;
+//	JTextField write;
+//	JScrollPane jp;
+//	static String cmsg="";
+//	public Server()
+//	{
+//		setBounds(400,100,500,500);
+//		getContentPane().setBackground(Color.blue);
+//		setDefaultCloseOperation(EXIT_ON_CLOSE);
+//		setLayout(null);
+//		message=new JTextArea();
+//		jp=new JScrollPane(message);
+//		jp.setBounds(50,50,400,200);
+//		write=new JTextField();
+//		write.setBounds(50, 270, 300, 30);
+//		add(jp);
+//		add(write);
+//		setVisible(true);
+//	}
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		new Server();
+		String ServerMessage;
+		Scanner input  = new Scanner(System.in);
 		ServerSocket ss=new ServerSocket(777);
 		System.out.println("Waiting for client");
 		Socket s=ss.accept();
+		while(true) {
 		BufferedReader br= new BufferedReader(new InputStreamReader(s.getInputStream()));
 		PrintWriter pw=new PrintWriter(s.getOutputStream());
 		String msg=br.readLine();
-		System.out.println("Client: "+msg);
-		pw.println("Hello");
+		if(msg!=null) {
+		System.out.println("Client:"+msg);
+		}
+		System.out.print("Server:");
+		ServerMessage = input.nextLine();
+		pw.println(ServerMessage);
 		pw.flush();
-		pw.close();
-		s.close();
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
+//		pw.close();
+		}
 		
+		//s.close();
 	}
 
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		/*if(e.getKeyCode()==KeyEvent.VK_LEFT) {
-			
-		}*/
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+//	@Override
+//	public void keyTyped(KeyEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void keyPressed(KeyEvent e) {
+//		// TODO Auto-generated method stub
+//		/*if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+//			
+//		}*/
+//	}
+//
+//	@Override
+//	public void keyReleased(KeyEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 
 }
